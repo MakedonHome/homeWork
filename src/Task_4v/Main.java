9019a9afe0e83694ed9ac17e1ecff56a2ee8d0ea
MakedonHome <71641245+MakedonHome@users.noreply.github.com>
2020-10-11 19:57:05 +0300

    Add files via upload

---
+++ src/Task_4v/Main.java
@@ -0,0 +1,11 @@
+package Task_4v;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int a = 25; a <= 35; a += 1) {
+            System.out.print(a+" ");
+            System.out.print(a + 0.5+" ");
+            System.out.println(a - 0.2);
+        }
+    }
+}
