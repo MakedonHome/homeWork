5ca71823552e8bcbb749272a9ba218d3897d813a
MakedonHome <71641245+MakedonHome@users.noreply.github.com>
2020-11-09 20:57:41 +0200

    Add files via upload

--- src/Loop_task10/Main.java
+++ src/Loop_task10/Main.java
@@ -2,19 +2,25 @@ package Loop_task10;
 
 //10.	Відомі результати двох спортсменів п’ятиборців в кожному з п’яти видів спорту в балах. Визначити, суму балів що одержав кожний спортсмен.
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner ff = new Scanner(System.in);
     public static void main(String[] args) {
-        int[] pars1 = {10,5,4,7,9};
-        int[] pars2 = {8,9,9,4,5};
         int sum = 0;
+        int sum2 = 0;
+        int pars1 = 0;
+        int pars2 = 0;
         for (int i = 0; i <5; i++) {
-            sum = sum +pars1[i];
+            pars1 = ff.nextInt();
+            sum = sum +pars1;
 
         }
         System.out.println(sum + " - сума балiв першого спортсмена");
-        sum = 0;
+
         for (int i = 0; i <5; i++) {
-            sum = sum +pars2[i];
+            pars2 = ff.nextInt();
+            sum = sum +pars2;
 
         }
         System.out.println(sum + " - сума балiв другого спортсмена");
--- src/Loop_task2/Main.java
+++ src/Loop_task2/Main.java
@@ -1,16 +1,23 @@
 package Loop_task2;
 
 
+import java.util.Scanner;
 
 public class Main {                                         // 2.	Відомі оцінки абітурієнта на п’яти екзаменах. Визначити скільки оцінок більше «9».
-    public static void main(String[] args){
+    static Scanner ff = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int mark = 0;
         int more10 = 0;
-        int[] marks = {12,2,10,8,4};
-        for (int i = 0; i <5; i++) {
-            if(marks[i]>9){
+        System.out.println("Введите оценки");
+        for (int i = 0; i < 5; i++) {
+            mark = ff.nextInt();
+            if (mark>9){
                 more10++;
-            }
         }
+        }
+
         System.out.println("Кiлькiсть оцiнок бiльше 9 - " + more10);
     }
+
 }
